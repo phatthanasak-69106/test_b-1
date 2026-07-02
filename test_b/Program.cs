dcf5085a769f2a46cdc0f57aba56d393b2d86942
test <littlepoy01@gmail.com>
2026-07-02 13:41:32 +0700

    Page 7

--- test_b/Program.cs
+++ test_b/Program.cs
@@ -1,14 +1,47 @@
-﻿double a, b, e, x, y;
-a = 1;
-b = 2;
-e = 1;
-x = 3;
-y = 2;
+﻿//declar
+string ID ;
+Double Sales, Salary, Commission, Rate, Bonus = 0, Total, Bonus_Spacial=0 ;
 
 
-var num1 = ((Math.Exp(x) * (Math.Sqrt((Math.Pow(a, 2)+(Math.Pow(b, x-y)))))));
-var num2 = ((2*Math.PI * Math.Pow(x, 2)))-(Math.Pow(y, 2));
-var result = num1 / num2;
+//input
+Console.Write("Enter ID : ");
+ID = Console.ReadLine();
 
+Console.Write("Enter Total : ");
+Total = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine($"Result: {result:F3}");
+Console.Write("Enter Sales : ");
+Sales = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter Salary : ");
+Salary = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter Rate 0-1 : ");
+Rate = Convert.ToDouble(Console.ReadLine());
+
+//process
+
+Commission = Sales * Rate;
+
+if (Total > 5)
+    {
+    Bonus = 9000;
+}
+
+if (Commission > Salary)
+{
+    Bonus_Spacial = Sales*0.1;
+}
+
+var Result = Sales + Commission + Bonus + Bonus_Spacial;
+
+//output
+
+Console.WriteLine($"-------------------");
+Console.WriteLine($"ID : {ID}");
+Console.WriteLine($"Sales : {Sales}");
+Console.WriteLine($"Salary : {Salary}");
+Console.WriteLine($"Bonus : {Bonus}");
+Console.WriteLine($"Bonus_Spacial : {Bonus_Spacial}");
+Console.WriteLine($"Commission : {Commission}");
+Console.WriteLine($"Result : {Result}");
